e2d47ff2de5ef1bcc12d98e2956cb896ba2a93f0
ThaisTimoteo <130317756+ThaisTimoteo@users.noreply.github.com>
2025-12-11 20:02:37 -0300

    Remove testHotfix method from tests
    Removed testHotfix method from WebAppApplicationTests.

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -243,10 +243,6 @@ class WebAppApplicationTests {
           .andExpect(content().string(containsString("Hello, Thais!")));
     }
     
-    @Test
-    public void testHotfix() {
-        assertEquals("hotfix funcionando!", homeController.hotfix());
-    }
     
     
 }
